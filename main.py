24d3cf62a43dc336561a1499839481a042e97455
Alex Feeley <alexandra.feeley.lamb@vanderbilt.edu>
2020-01-30 12:54:55 -0600

    testing

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 def main():
     print("Hello World!")
-<<<<<<< HEAD
     print("Alex knows more than me")
-
-
-=======
     print("time to get crackalackin")
->>>>>>> 85bc60420328d9cdd4ab4529fb1dd48f1e12d08c
 main()
